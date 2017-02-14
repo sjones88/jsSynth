9c23a91ffcb1bb2e99683acf88d14b17eb2d2bf8
sjones88 <steven.g.jones88@gmail.com>
2017-02-14 14:55:20 -0600

    more functions

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ let masterGainNode = null;
 let keyboard = document.querySelector(".keyboard");
 let wavePicker = document.querySelector("select[name='waveform']");
 let volumeControl = document.querySelector("input[name='volume']");
-let noteFreq = [];
+let noteFreq = null;
 let customWaveform = null;
 let sineTerms = null;
 let cosineTerms = null;
 
 function createNoteTable() {
+	noteFreq = [];
 	let a = 2 ** (1/12);
 	let n = -57;
 	let notes = ["C", "C#", "D", "D#", "E",
@@ -25,4 +26,75 @@ function createNoteTable() {
 
 	return noteFreq;
 }
+
+function createKey(note, octave, freq) {
+	let keyElement = document.createElement('div');
+	let labelElement = document.createElement('div');
+
+	if (note.length === 1) {
+		keyElement.className = "key";
+	} else {
+		keyElement.className = "black key";
+	}
+	keyElement.dataset["octave"] = octave;
+	keyElement.dataset["note"] = note;
+	keyElement.dataset["frequency"] = freq;
+
+	labelElement.innerHTML = note + "<sub>" + octave + "</sub>";
+	keyElement.appendChild(labelElement);
+
+	keyElement.addEventListener("mousedown", notePressed, false);
+	keyElement.addEventListener("mouseup", noteReleased, false);
+	keyElement.addEventListener("mouseover", notePressed, false);
+	keyElement.addEventListener("mouseleave", noteReleased, false);
+
+	return keyElement;
+}
+
+var changeVolume = (event) => {
+	masterGainNode.gain.value = volumeControl.value;
+};
+
+function setup() {
+	noteFreq = createNoteTable();
+
+	volumeControl.addEventListener("change", changeVolume, false);
+
+	masterGainNode = audioContext.createGain();
+	masterGainNode.connect(audioContext.destination);
+	masterGainNode.gain.value = volumeControl.value;
+
+	noteFreq.forEach((keys, idx) => {
+		let keyList = Object.entries(keys);
+		console.log(keyList);
+		let octaveElem = document.createElement("div");
+		octaveElem.className = "octave";
+
+		keyList.forEach((key) => {
+			octaveElem.appendChild(createKey(key[0], idx, key[1]));
+		});
+
+		keyboard.appendChild(octaveElem);
+	});
+
+	document.querySelector("div[data-note='B'][data-octave='5']").scrollIntoView(false);
+
+	sineTerms = new Float32Array([0, 0, 1, 0, 1]);
+	cosineTerms = new Float32Array(sineTerms.length);
+	customWaveform = audioContext.createPeriodicWave(cosineTerms, sineTerms);
+
+	for (i=0; i<9; i++) {
+		oscList[i] = [];
+	}
+}
+
+var playTone = (freq) => {
+	let osc = audioContext.createOscillator(); // create a new OscillatorNode
+	osc.connect(masterGainNode); // tell OscillatorNode where to send its output
+
+};
+
+setup();
+
+
 
